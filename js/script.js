b0bf67be984a8a21909d718e026d40ba5736b363
Hien <phanhien01212@gmail.com>
2021-10-10 13:42:04 +0700

    update

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ $('.slider').slick({
     infinite: true,
     arrows: false,
     dots: true,
-    // autoplay: true,
+    autoplay: true,
     autoplaySpeed: 3000,
     fade: true,
     cssEase: 'linear',
